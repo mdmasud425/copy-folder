eaffe828d9e23d973554ae0a0c43d0fd9fb461bf
mdmasud425 <siamrana39525@Gmail.com>
2024-09-08 15:49:21 +0600

    03.40

---
+++ assets/js/style.js
@@ -0,0 +1,22 @@
+
+// function one() {
+//     return new Promise(function(resolv, reject){
+//         resolv('hello one');
+//     });
+// }
+
+// function two(){
+//     return new Promise(function(resolv, reject){
+//         resolv('hello two');
+//     });
+// }
+
+// one()
+//     .then(function(res) {
+//         console.log(res);
+
+//         return two();
+//     })
+//     .then(function(res) {
+//         console.log(res);
+//     })
